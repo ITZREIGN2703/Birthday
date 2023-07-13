b225b62ed0cd209e42a6ea94969e471a2614faf6
ITZREIGN2703 <129649856+ITZREIGN2703@users.noreply.github.com>
2023-07-13 20:24:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function goToNextPage() 
+{
+  window.location.href = "next.html";
+}
+
+function goToPage()
+{
+  window.location.href = "another.html";
+}
+
+function goToPrev()
+{
+  window.location.href = "index.html";
+}
